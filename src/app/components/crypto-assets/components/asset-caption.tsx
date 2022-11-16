3a4eed7ba245924398b25292612febb4149c4398
kyranjamie <kyran.burraston@gmail.com>
2022-11-16 15:14:22 +0100

    chore: fix microblock caption alignment issue

--- src/app/components/crypto-assets/components/asset-caption.tsx
+++ src/app/components/crypto-assets/components/asset-caption.tsx
@@ -1,6 +1,6 @@
 import { FiInfo } from 'react-icons/fi';
 
-import { Box, Flex, Stack, Tooltip, color } from '@stacks/ui';
+import { Box, Flex, Tooltip, color } from '@stacks/ui';
 
 import { Caption } from '@app/components/typography';
 
@@ -10,28 +10,24 @@ interface AssetCaptionProps {
 }
 export function AssetCaption({ caption, isUnanchored }: AssetCaptionProps) {
   return (
-    <Flex flexDirection="row">
-      <Caption lineHeight="1.5">{caption}</Caption>{' '}
+    <Flex flexDirection="row" alignItems="center">
+      <Caption>{caption}</Caption>{' '}
       {isUnanchored ? (
         <>
-          <Caption ml={1}> • Microblock </Caption>
+          <Caption ml={1}> • Microblock</Caption>
           <Tooltip placement="right-end" label={'Learn more about microblocks'}>
-            <Stack isInline>
-              <a href="https://docs.stacks.co/understand-stacks/microblocks" target="_blank">
-                <Box
-                  _hover={{ cursor: 'pointer' }}
-                  size="12px"
-                  color={color('text-caption')}
-                  as={FiInfo}
-                  ml={1}
-                />
-              </a>
-            </Stack>
+            <a href="https://docs.stacks.co/understand-stacks/microblocks" target="_blank">
+              <Box
+                _hover={{ cursor: 'pointer' }}
+                size="12px"
+                color={color('text-caption')}
+                as={FiInfo}
+                ml={1}
+              />
+            </a>
           </Tooltip>
         </>
-      ) : (
-        ''
-      )}
+      ) : null}
     </Flex>
   );
 }
